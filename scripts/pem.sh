8909dffe71c93c6941d56e004cc7c42f07e1286f
rafaellunaM <rafael.luna.cavalcanti@gmail.com>
2025-06-01 22:33:46 -0300

    --amend-no-edit

--- scripts/pem.sh
+++ scripts/pem.sh
@@ -3,17 +3,17 @@
 set -e
 cat orderermsp.yaml | grep -A 100 "pem: |" | sed 's/.*pem: |//' | sed '/^[[:space:]]*$/d' | sed 's/^[[:space:]]*//' > /tmp/orderer-cert.pem
 
-kubectl hlf channelcrd main create \
-  --name demo \
-  --channel-name demo \
-  --secret-name wallet \
-  --admin-orderer-orgs OrdererMSP \
-  --orderer-orgs OrdererMSP \
-  --identities "OrdererMSP;orderermsp.yaml" \
-  --identities "OrdererMSP-sign;orderermspsign.yaml" \
-  --admin-peer-orgs Org1MSP \
-  --peer-orgs Org1MSP \
-  --identities "Org1MSP;org1msp.yaml" \
-  --secret-ns default \
-  --consenters "orderer0-ord.localho.st:443" \
-  --consenter-certificates /tmp/orderer-cert.pem
+# kubectl hlf channelcrd main create \
+#   --name demo \
+#   --channel-name demo \
+#   --secret-name wallet \
+#   --admin-orderer-orgs OrdererMSP \
+#   --orderer-orgs OrdererMSP \
+#   --identities "OrdererMSP;orderermsp.yaml" \
+#   --identities "OrdererMSP-sign;orderermspsign.yaml" \
+#   --admin-peer-orgs Org1MSP \
+#   --peer-orgs Org1MSP \
+#   --identities "Org1MSP;org1msp.yaml" \
+#   --secret-ns default \
+#   --consenters "orderer0-ord.localho.st:443" \
+#   --consenter-certificates /tmp/orderer-cert.pem
